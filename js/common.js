bc022c8a680957e9d073b9d7eee01c8d4a13f99a
Oleksandr Fentsyk <61778081+OFenstyk@users.noreply.github.com>
2020-06-09 17:21:30 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
 		slidesPerView: 4,
 		loop: true,
 		freeMode: true,
-		loopedSlides: 5, //looped slides should be the same
+		loopedSlides: 5, 
 		watchSlidesVisibility: true,
 		watchSlidesProgress: true,
 	  });
 	  var galleryTop = new Swiper('.gallery-top', {
 		spaceBetween: 10,
 		loop:true,
-		loopedSlides: 5, //looped slides should be the same
+		loopedSlides: 5, 
 		navigation: {
 		  nextEl: '.swiper-button-next',
 		  prevEl: '.swiper-button-prev',
